144207fd51e64c793845808a54450d21e737af7f
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-02-23 16:52:32 +0900

    feat: create Board Entity

---
+++ src/main/java/com/elice/boardproject/post/domain/Post.java
@@ -0,0 +1,27 @@
+package com.elice.boardproject.post.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    private String title;
+    private String content;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateDate;
+
+}
