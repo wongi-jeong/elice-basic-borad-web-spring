b5e99ddbb5fd38a503293b34557fa3cbde4a42d4
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-02-24 19:16:46 +0900

    feat: add createAt field

--- src/main/java/com/elice/boardproject/board/domain/Board.java
+++ src/main/java/com/elice/boardproject/board/domain/Board.java
@@ -2,13 +2,17 @@ package com.elice.boardproject.board.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
-import java.util.Date;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Builder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,6 +23,10 @@ public class Board {
 
     @Column(length = 200)
     private String description;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime createAt;
 
     public Board(String name, String description) {
         this.name = name;
