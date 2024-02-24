73ceaf634f0ee5fefe609d152140260c740ac213
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-02-24 20:00:05 +0900

    refactor: add requestMapping, boardMapper, boardPostDTO

--- src/main/java/com/elice/boardproject/board/controller/BoardController.java
+++ src/main/java/com/elice/boardproject/board/controller/BoardController.java
@@ -1,85 +1,86 @@
 package com.elice.boardproject.board.controller;
 
 import com.elice.boardproject.board.domain.Board;
-import com.elice.boardproject.board.dto.BoardDTO;
+import com.elice.boardproject.board.dto.BoardPostDTO;
 import com.elice.boardproject.board.service.BoardService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.elice.boardproject.board.mapper.BoardMapper;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.extern.slf4j.Slf4j;
+
 
 @Slf4j
 @Controller
-public class BoardController{
+@RequestMapping("boards")
+public class BoardController {
 
     private final BoardService boardService;
+    private final BoardMapper boardMapper;
 
-    @Autowired
-    public BoardController(BoardService boardService) {
+
+    public BoardController(BoardService boardService, BoardMapper boardMapper) {
         this.boardService = boardService;
+        this.boardMapper = boardMapper;
     }
 
     // 게시판 목록 조회
-    @GetMapping("/boards")
+    @GetMapping
     public String showBoardList(Model model) {
         model.addAttribute("boards", this.boardService.getAllBoards());
+
         return "board/boards";
     }
 
+    // 게시판 조회
+    @GetMapping("{id}")
+    public String getBorad(@PathVariable Long id, Model model) {
+        model.addAttribute("board", boardService.getBoardById(id));
+
+        return "board/board";
+    }
+
     // 게시판 생성 페이지 조회
-    @GetMapping("/boards/create")
+    @GetMapping("/create")
     public String newBoard() {
         return "board/createBoard";
     }
 
+    // 게시판 생성
+    @PostMapping("/boards/create")
+    public String createBoard(@ModelAttribute BoardPostDTO boardPostDTO) {
+        Board board = boardMapper.boardPostDTOBoard(boardPostDTO);
+        boardService.saveBoard(board);
 
-    // 게시판 조회
-    @GetMapping("/boards/{id}")
-    public String getBorad(@PathVariable Long id, Model model) {
-        model.addAttribute("board", boardService.getBoardById(id));
-        return "board/board";
+        return "redirect:/boards";
     }
 
     // 게시판 수정 페이지 조회
-    @GetMapping("boards/edit/{id}")
+    @GetMapping("/{id}/edit")
     public String editBoard(@PathVariable Long id, Model model) {
         model.addAttribute("board", boardService.getBoardById(id));
+
         return "board/editBoard";
     }
 
-    // 게시판 생성
-    @PostMapping("/boards/create")
-    public String createBoard(@ModelAttribute BoardDTO boardDTO) {
-        Board board = Board.builder()
-                .title(boardDTO.getTitle())
-                .content(boardDTO.getContent())
-                .build();
-
-        Long boardId = boardService.saveBoard(board);
-
-        return "redirect:/boards/" + boardId;
-    }
 
     // 게시판 수정
-    @PutMapping("/boards/edit/{id}")
-    public String editBoard(@PathVariable Long id, @ModelAttribute BoardDTO boardDTO) {
-        Board board = Board.builder()
-                .boardId(id)
-                .title(boardDTO.getTitle())
-                .content(boardDTO.getContent())
-                .build();
+    @PutMapping("/{id}/edit")
+    public String editBoardPost(@PathVariable Long id, @ModelAttribute BoardPostDTO boardPostDTO) {
+        Board board = boardMapper.boardPostDTOBoard(boardPostDTO).toBuilder().id(id).build();
         boardService.updateBoard(board);
 
-        return "redirect:/boards/" + id;
+        return "redirect:/boards";
     }
 
     // 게시판 삭제
-    @DeleteMapping("/boards/{id}")
+    @DeleteMapping("/{id}/delete")
     public String deleteBoard(@PathVariable Long id) {
         boardService.deleteBoardById(id);
-        return "redirect:/boards/";
+
+        return "redirect:/boards";
     }
 
 }
