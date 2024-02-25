b03c4737b5dd1bf9a16736b6c44e655fe2b8453d
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-02-25 19:53:07 +0900

    refactor: add page methods

--- src/main/java/com/elice/boardproject/post/repsoitory/PostRepository.java
+++ src/main/java/com/elice/boardproject/post/repsoitory/PostRepository.java
@@ -1,10 +1,18 @@
 package com.elice.boardproject.post.repsoitory;
 
+import com.elice.boardproject.board.domain.Board;
 import com.elice.boardproject.post.domain.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    // 생성 일자의 내림차순으로 조회된 모든 게시판 객체를 Page로 넘겨주기
+    Page<Post> findAllByBoardOrderByCreateAtDesc(Board board, Pageable pageable);
+
+    // 검색 키워드에 맞는 게시판으로 조회된 모든 게시판 객체를 Page로 넘겨주기
+    Page<Post> findAllByBoardAndTitleContaining(Board board, String keyword, Pageable pageable);
 }
