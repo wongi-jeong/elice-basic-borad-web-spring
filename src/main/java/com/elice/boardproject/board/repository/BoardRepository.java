c9fb13dfedbef5e8eac052d2416af93d40ae00bf
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-02-25 18:08:12 +0900

    refactor: remove unnecessary imports

--- src/main/java/com/elice/boardproject/board/repository/BoardRepository.java
+++ src/main/java/com/elice/boardproject/board/repository/BoardRepository.java
@@ -9,7 +9,6 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
-import javax.swing.plaf.basic.BasicTreeUI;
 import java.sql.PreparedStatement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
