a6ffa96a016494f8645d40618ff73592db0549d1
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-02-25 18:24:12 +0900

    refactor: remove built-in abstract methods

--- src/main/java/com/elice/boardproject/post/repsoitory/PostRepository.java
+++ src/main/java/com/elice/boardproject/post/repsoitory/PostRepository.java
@@ -4,16 +4,7 @@ import com.elice.boardproject.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post>findAll();
 
-    Optional<Post>findById(Long id);
-
-    Post save(Post post);
-
-    void delete(Post post);
 }
