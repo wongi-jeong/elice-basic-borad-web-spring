b82039c48ec74e85b5849eee1dd9e2988802156c
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-02-27 21:15:31 +0900

    rename: fix package name, query methods name

--- src/main/java/com/elice/boardproject/post/repository/PostRepository.java
+++ src/main/java/com/elice/boardproject/post/repository/PostRepository.java
@@ -1,7 +1,7 @@
-package com.elice.boardproject.post.repsoitory;
+package com.elice.boardproject.post.repository;
 
-import com.elice.boardproject.board.domain.Board;
-import com.elice.boardproject.post.domain.Post;
+import com.elice.boardproject.board.entity.Board;
+import com.elice.boardproject.post.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Repository;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     // 생성 일자의 내림차순으로 조회된 모든 게시판 객체를 Page로 넘겨주기
-    Page<Post> findAllByBoardOrderByCreateAtDesc(Board board, Pageable pageable);
+    Page<Post> findAllByBoardOrderByBoardCreateAtDesc(Board board, Pageable pageable);
 
     // 검색 키워드에 맞는 게시판으로 조회된 모든 게시판 객체를 Page로 넘겨주기
     Page<Post> findAllByBoardAndTitleContaining(Board board, String keyword, Pageable pageable);
