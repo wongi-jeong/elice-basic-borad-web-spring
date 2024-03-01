99f35fe8f2e0683728c6261b70afcfb9006de97c
[Cloud2] 정원기 <elicestudy0525@gmail.com>
2024-03-01 11:11:24 +0900

    feat: add comment controller file

---
+++ src/main/java/com/elice/boardproject/comment/controller/CommentController.java
@@ -0,0 +1,41 @@
+package com.elice.boardproject.comment.controller;
+
+import com.elice.boardproject.comment.dto.CommentDTO;
+import com.elice.boardproject.comment.entity.Comment;
+import com.elice.boardproject.comment.mapper.CommentMapper;
+import com.elice.boardproject.comment.service.CommentService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/comments")
+public class CommentController {
+    private final CommentService commentService;
+    private final CommentMapper commentMapper;
+
+    @PostMapping
+    public String createComment(@ModelAttribute CommentDTO commentDTO, @RequestParam Long postId, RedirectAttributes redirectAttributes) {
+        Comment comment = commentMapper.commentDtoToComment(commentDTO);
+        commentService.createComment(postId, comment);
+        redirectAttributes.addAttribute("postId", postId);
+        return "redirect:/posts/{postId}";
+    }
+
+    @PostMapping("/{commentId}/edit")
+    public String editComment(@PathVariable Long commentId, @ModelAttribute CommentDTO commentDTO, RedirectAttributes redirectAttributes) {
+        Comment comment = commentMapper.commentDtoToComment(commentDTO);
+        Comment updateComment = commentService.updateComment(commentId, comment);
+
+        redirectAttributes.addAttribute("postId", updateComment.getPost().getId());
+        return "redirect:/posts/{postId}";
+    }
+
+    @DeleteMapping("/{commentId}")
+    public String deleteComment(@PathVariable Long commentId) {
+        commentService.deleteComment(commentId);
+        return "redirect:/posts";
+    }
+}
